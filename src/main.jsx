2c5147736c3179ca6ec88da0984cc4176db42fa9
sara youssef <154534692+sarayoutest@users.noreply.github.com>
2025-09-20 16:12:00 -0700

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter  } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import StoreConstextProvider from "./Context/StoreContext.jsx";
+
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
-  <StoreConstextProvider>
-    <App />
-  </StoreConstextProvider>
-  </HashRouter >
+  <BrowserRouter basename="/repo-name">
+    <StoreConstextProvider>
+      <App />
+    </StoreConstextProvider>
+  </BrowserRouter>
 );
