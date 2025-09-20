356b2a937089d5e16adf80339baca522a036a190
sara youssef <154534692+sarayoutest@users.noreply.github.com>
2025-09-20 14:54:23 -0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
 import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cart from "./pages/Cart/Cart";
 import Placeorder from "./pages/Placeorder/Placeorder";
 import Footer from "./components/Footer/Footer";
 import Login from "./components/Login/Login";
-import AppRoutes from "./AppRoutes";
 
 function App() {
   const [showLogin, setShowLogin] = useState(false);
+
   return (
-    <>
+    <BrowserRouter basename="/food-delivery">
       {showLogin && <Login setShowLogin={setShowLogin} />}
-      <BrowserRouter basename="/food-delivery">
-        <div className="app">
-          <Navbar setShowLogin={setShowLogin} />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/order" element={<Placeorder />} />
-            <Route path="/*" element={<AppRoutes />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
-    </>
+      <Navbar setShowLogin={setShowLogin} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<Placeorder />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
